39cd06af611fcff6e7cbf06eb2a911196eec0097
ben <ben.raine@engineering.digital.dwp.gov.uk>
2023-11-23 15:35:11 +0000

    first push

---
+++ app/routes.js
@@ -0,0 +1,19 @@
+//
+// For guidance on how to create routes see:
+// https://prototype-kit.service.gov.uk/docs/create-routes
+//
+
+const govukPrototypeKit = require('govuk-prototype-kit')
+const router = govukPrototypeKit.requests.setupRouter()
+
+// Add your routes here
+
+// GENERIC NEXT PAGE ELEMENT
+router.post('*', function (req, res, next) {
+    console.log(req.body);
+    if (req.body['next-page']) {
+      res.redirect(req.body['next-page']);
+    } else {
+      next();
+    }
+  });
